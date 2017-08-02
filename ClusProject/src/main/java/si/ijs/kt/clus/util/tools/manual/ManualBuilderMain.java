7d21df9f7d3002d77d62c99f6ab3d07c70e93001
Matej Petkovic <matej@petkovic.ijs.si>
2017-08-02 18:43:41 +0200

    forgot to include one file

--- ClusProject/src/main/java/si/ijs/kt/clus/util/tools/manual/ManualBuilderMain.java
+++ ClusProject/src/main/java/si/ijs/kt/clus/util/tools/manual/ManualBuilderMain.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ManualBuilderMain {
     public static final String MANUAL_DIR = "docs/manual/";
-    public static final String MANUAL_OPTIONS_LISTS_DIR = MANUAL_DIR + "optionLists";
+    public static final String MANUAL_OPTIONS_LISTS_DIR = MANUAL_DIR + "optionListsHatchery";
 
     public static void main(String[] args) throws IOException {   
         ManualBuilder mb = new ManualBuilder();
