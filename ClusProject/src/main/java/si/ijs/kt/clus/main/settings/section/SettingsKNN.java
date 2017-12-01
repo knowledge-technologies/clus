226d4f25c9f1063dcccf0582ffb1858bbf41f896
Matej Petkovic <matej@petkovic.ijs.si>
2017-12-01 14:56:31 +0100

    moved some relief settings to [kNN] section (load from files ...)

--- ClusProject/src/main/java/si/ijs/kt/clus/main/settings/section/SettingsKNN.java
+++ ClusProject/src/main/java/si/ijs/kt/clus/main/settings/section/SettingsKNN.java
@@ -1,7 +1,10 @@
 
 package si.ijs.kt.clus.main.settings.section;
 
+import java.util.Arrays;
+
 import si.ijs.kt.clus.main.settings.SettingsBase;
+import si.ijs.kt.clus.util.jeans.io.ini.INIFileArray;
 import si.ijs.kt.clus.util.jeans.io.ini.INIFileEnum;
 import si.ijs.kt.clus.util.jeans.io.ini.INIFileNominalOrIntOrVector;
 import si.ijs.kt.clus.util.jeans.io.ini.INIFileSection;
@@ -27,7 +30,7 @@ public class SettingsKNN extends SettingsBase {
     public enum Distance {Euclidean, Chebyshev, Manhattan};
     private INIFileEnum<Distance> m_Distance;
     
-    public enum SearchMethod {BrutForce, VPTree, KDTree};
+    public enum SearchMethod {BruteForce, VPTree, KDTree, Oracle};
     private INIFileEnum<SearchMethod> m_SearchMethod;	
     
     public final static String[] DISTANCE_WEIGHTS = new String[]{"constant", "1/d", "1-d"};
@@ -37,6 +40,15 @@ public class SettingsKNN extends SettingsBase {
     private INIFileNominalOrIntOrVector m_distanceWeight;
     
     private INIFileString m_attributeWeight; // parsed in KnnModel for its very diverse uses
+    
+    private INIFileNominalOrIntOrVector m_ChosenInstancesTrain;
+    private INIFileNominalOrIntOrVector m_ChosenInstancesTest;
+    public static int[] DUMMY_INSTANCES = new int[] {-1};
+    
+    private INIFileArray m_LoadNeighboursFiles;
+    private INIFileString m_SaveNeighboursFile;
+    private static String EMPTY_STRING = "";
+    private static String[] EMPTY_STRING_ARRAY = new String[] {};
 
 
     public int[] getKNNk() {
@@ -73,7 +85,77 @@ public class SettingsKNN extends SettingsBase {
     	return m_SearchMethod.getChosenOption();
     }
 
+    
+    public int[] getChosenIntancesTrain(int nbTrainInstances) {
+    	int[] inSettings = m_ChosenInstancesTrain.getIntVector();
+    	if(Arrays.equals(inSettings, DUMMY_INSTANCES)) {
+    		return rangeInterval(nbTrainInstances);
+    	} else {
+    		return inSettings;
+    	}
+    }
+    
+    public int[] getChosenIntancesTest(int nbTestInstances) {
+    	int[] inSettings = m_ChosenInstancesTest.getIntVector();
+    	if(Arrays.equals(inSettings, DUMMY_INSTANCES)) {
+    		return rangeInterval(nbTestInstances);
+    	} else {
+    		return inSettings;
+    	}
+    }
    
+    public boolean shouldSaveNeighbours() {
+    	return !m_SaveNeighboursFile.getValue().equals(EMPTY_STRING);
+    }
+    
+    public boolean shouldLoadNeighbours() {
+    	return m_LoadNeighboursFiles.getSize() > 0;
+    }
+    
+    public String[] getLoadNeighboursFiles() {
+    	String[] files = new String[m_LoadNeighboursFiles.getSize()];
+    	for(int i = 0; i < files.length; i++) {
+    		files[i] = m_LoadNeighboursFiles.getStringAt(i);
+    	}
+    	return files;
+    }
+    
+    public String getSaveNeighboursFile() {
+    	return m_SaveNeighboursFile.getValue();
+    }
+    
+    public boolean mustNotComputeTrainingError(int nbTrainInstances) {
+    	return mustNotComputeError(nbTrainInstances, getChosenIntancesTrain(nbTrainInstances));
+    }
+    
+    public boolean mustNotComputeTestError(int nbTestInstances) {
+    	return mustNotComputeError(nbTestInstances, getChosenIntancesTest(nbTestInstances));
+    }
+    
+    private static boolean mustNotComputeError(int nb_instances, int[] chosenInstances) {
+    	Arrays.sort(chosenInstances);
+    	int[] range = new int[nb_instances];
+    	for (int i = 0; i < nb_instances; i++) {
+    		range[i] = i;
+    	}
+    	return !Arrays.equals(chosenInstances, range);
+    }
+    
+    private int[] getChosenInstances(int nbInstances, int[] chosenInstances) {
+    	if (Arrays.equals(chosenInstances, DUMMY_INSTANCES)) {
+    		return rangeInterval(nbInstances);
+    	} else {
+    		return chosenInstances;
+    	}
+    }
+    
+    private int[] rangeInterval(int upperBound) {
+    	int[] interval = new int[upperBound];
+    	for (int i = 0; i < upperBound; i++) {
+    		interval[i] = i;
+    	}
+    	return interval;
+    }
 
 
     @Override
@@ -84,11 +166,20 @@ public class SettingsKNN extends SettingsBase {
         
         m_SectionKNN.addNode(m_Distance = new INIFileEnum<Distance>("Distance", Distance.class, Distance.Euclidean));
         //m_SectionKNN.addNode(m_searchMethod = new INIFileNominal("SearchMethod", SEARCH_METHODS, SEARCH_METHOD_BRUTE_FORCE));
-        m_SectionKNN.addNode(m_SearchMethod =  new INIFileEnum<SearchMethod>("SearchMethod", SearchMethod.class, SearchMethod.BrutForce));
+        m_SectionKNN.addNode(m_SearchMethod =  new INIFileEnum<SearchMethod>("SearchMethod", SearchMethod.class, SearchMethod.BruteForce));
         m_SectionKNN.addNode(m_distanceWeight = new INIFileNominalOrIntOrVector("DistanceWeighting", DISTANCE_WEIGHTS));        
         m_distanceWeight.setNominal(DISTANCE_WEIGHTING_CONSTANT);
         
         m_SectionKNN.addNode(m_attributeWeight = new INIFileString("AttributeWeighting", "none"));
+        
+        m_SectionKNN.addNode(m_ChosenInstancesTrain = new INIFileNominalOrIntOrVector("ChosenInstancesTrain", NONELIST));
+        m_ChosenInstancesTrain.setIntVector(DUMMY_INSTANCES);
+        m_SectionKNN.addNode(m_ChosenInstancesTest = new INIFileNominalOrIntOrVector("ChosenInstancesTest", NONELIST));
+        m_ChosenInstancesTest.setIntVector(DUMMY_INSTANCES);
+        m_SectionKNN.addNode(m_LoadNeighboursFiles = new INIFileArray("LoadNeighboursFiles", EMPTY_STRING_ARRAY));
+        m_SectionKNN.addNode(m_SaveNeighboursFile = new INIFileString("SaveNeighboursFile", EMPTY_STRING));
+        
+        
         m_SectionKNN.setEnabled(false);
 
         return m_SectionKNN;
--- ClusProject/src/main/java/si/ijs/kt/clus/main/settings/section/SettingsRelief.java
+++ ClusProject/src/main/java/si/ijs/kt/clus/main/settings/section/SettingsRelief.java
@@ -32,18 +32,9 @@ public class SettingsRelief extends SettingsBase {
     private INIFileNominalOrDoubleOrVector m_ReliefNbIterations;
     private INIFileBool m_ReliefShouldHaveNeighbourWeighting;
     private INIFileDouble m_ReliefWeightingSigma;
-//    private INIFileBool m_ReliefLoadNeighboursFromFile;
-//    private INIFileBool m_ReliefSaveNeighboursToFile;
-    private INIFileArray m_ReliefLoadNeighboursFiles;
-    private INIFileString m_ReliefSaveNeighboursFile;
-    private static String EMPTY_STRING = "";
-    private static String[] EMPTY_STRING_ARRAY = new String[] {};
     
     private INIFileNominalOrIntOrVector m_ChosenInstances;
     public static int[] DUMMY_INSTANCES = new int[] {-1};
-    
-    
-//    private INIFileString m_NeighboursFile;
     
 	// If new types are introduced use the same names as in SettingsMLC class
     public enum MultilabelDistance {HammingLoss, MLAccuracy, MLFOne, SubsetAccuracy};
@@ -135,10 +126,7 @@ public class SettingsRelief extends SettingsBase {
     }
     
     
-    public boolean shouldSaveNeighbours() {
-//    	return m_ReliefSaveNeighboursToFile.getValue();
-    	return !m_ReliefSaveNeighboursFile.getValue().equals(EMPTY_STRING);
-    }
+
     
     public int[] getChosenIntances() {
     	return m_ChosenInstances.getIntVector();
@@ -148,22 +136,6 @@ public class SettingsRelief extends SettingsBase {
 //    	m_ReliefSaveNeighboursToFile.setValue(false);
 //    }
     
-    public boolean shouldLoadNeighbours() {
-//    	return m_ReliefLoadNeighboursFromFile.getValue();
-    	return m_ReliefLoadNeighboursFiles.getSize() > 0;
-    }
-    
-    public String[] getLoadNeighboursFiles() {
-    	String[] files = new String[m_ReliefLoadNeighboursFiles.getSize()];
-    	for(int i = 0; i < files.length; i++) {
-    		files[i] = m_ReliefLoadNeighboursFiles.getStringAt(i);
-    	}
-    	return files;
-    }
-    
-    public String getSaveNeighboursFile() {
-    	return m_ReliefSaveNeighboursFile.getValue();
-    }
     
     public MultilabelDistance getMultilabelDistance() {
     	return m_MultilabelDistance.getChosenOption();
@@ -193,10 +165,6 @@ public class SettingsRelief extends SettingsBase {
                                                                                                     // the authors do
                                                                                                     // not give any
                                                                                                     // suggestions
-//        m_SectionRelief.addNode(m_ReliefSaveNeighboursToFile = new INIFileBool("SaveNeighboursToFile", false));
-//        m_SectionRelief.addNode(m_ReliefLoadNeighboursFromFile = new INIFileBool("LoadNeighboursFromFile", false));
-        m_SectionRelief.addNode(m_ReliefLoadNeighboursFiles = new INIFileArray("LoadNeighboursFiles", EMPTY_STRING_ARRAY));
-        m_SectionRelief.addNode(m_ReliefSaveNeighboursFile = new INIFileString("SaveNeighboursFile", EMPTY_STRING));
         m_SectionRelief.addNode(m_ChosenInstances = new INIFileNominalOrIntOrVector("ChosenInstances", NONELIST));
         m_ChosenInstances.setIntVector(DUMMY_INSTANCES);
         m_SectionRelief.addNode(m_MultilabelDistance =  new INIFileEnum<MultilabelDistance>("MultilabelDistance", MultilabelDistance.class, MultilabelDistance.HammingLoss));
